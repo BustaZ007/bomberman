7c220f2ea4f3dd32e3752ca8c0957da8510ecf7f
Unknown <pavel.zaborin@inbox.ru>
2018-04-15 23:13:38 +0500

    Ура, оно шевелится

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+
+    [SerializeField]
+    public float speed;
+    [SerializeField]
+    private Animator animator;
+    private bool flag;
+	// Use this for initialization
+	void Start () 
+    {
+        flag = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed, Space.Self);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.tag == "Wall" || collision.tag == "Block")
+        {
+			speed = -speed;
+			//flip();
+        }
+	}
+    void flip()
+    {
+        animator.GetBool("false");
+    }
+}
---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public GameObject player;
+    private BoxCollider2D box;
+	void Start ()
+    {
+        //gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        Invoke("Boom", 3.0f);
+	}
+    void Boom()
+    {
+        
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        if (transform.position !=
+           new Vector3(Mathf.RoundToInt(player.transform.position.x),
+                       Mathf.RoundToInt(player.transform.position.y)))
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+	}
+}

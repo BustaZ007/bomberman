64a7713a96d2a74eed8a8118eb3feac417878984
Unknown <lhfujy7477@gmail.com>
2018-04-26 21:05:27 +0500

    add MonsterDrop

---
+++ Assets/Scripts/DropMove.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropMove : MonoBehaviour {
+    [SerializeField]
+    float speed;
+    Animator animator;
+    public GameObject player;
+    bool move;
+    int count = 0;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        move = true;
+    }
+
+    void Update()
+    {
+        Collider2D[] colLeft = Physics2D.OverlapCircleAll(transform.position - transform.right, 0.487f);
+        Collider2D[] colRight = Physics2D.OverlapCircleAll(transform.position + transform.right, 0.487f);
+        Collider2D[] colUp = Physics2D.OverlapCircleAll(transform.position + transform.up, 0.487f);
+        Collider2D[] colDown = Physics2D.OverlapCircleAll(transform.position - transform.up, 0.487f);
+
+        if (move)
+            if (colUp.Length == 0 || colDown.Length == 0)
+            {
+                if (count == 4)
+                {
+                    transform.Translate(Vector2.down * Time.deltaTime * speed, Space.Self);
+                    count = 0;
+                    move = false;
+                }
+                else
+                {
+                    count++;
+                    transform.Translate(Vector2.right * Time.deltaTime * speed, Space.Self);
+                }
+            }
+            else
+                transform.Translate(Vector2.right * Time.deltaTime * speed, Space.Self);
+        else
+            if (colLeft.Length == 0 || colRight.Length == 0)
+        {
+            if (count == 4)
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * speed, Space.Self);
+                count = 0;
+                move = true;
+            }
+            else
+            {
+                count++;
+                transform.Translate(Vector2.down * Time.deltaTime * speed, Space.Self);
+            }
+        }
+        else
+            transform.Translate(Vector2.down * Time.deltaTime * speed, Space.Self);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Wall" || collision.tag == "Block" ||
+            collision.tag == "Bomb" || collision.tag == "Monster")
+        {
+            speed = -speed;
+            if (speed < 0)
+                animator.SetInteger("flag", -1);
+            else
+                animator.SetInteger("flag", 1);
+        }
+        if (collision.tag == "Fire")
+        {
+            speed = 0;
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            animator.SetInteger("flag", 2);
+            Destroy(gameObject, 3f);
+        }
+    }
+}

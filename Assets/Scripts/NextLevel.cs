4f6a55a2d5184e5c97b6bb36b3b8b636c4677ecd
Unknown <lhfujy7477@gmail.com>
2018-04-29 21:58:50 +0500

    add transition on next level

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+
+    public bool isReady = false;
+    int level = 0;
+
+    //void  OnTriggerEnter( Collider col)
+    //{
+    //    if (col.tag == "Player") isReady = true;
+    //    level++;
+    //}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && isReady) SceneManager.LoadScene("Level2");
+    }
+
+
+    void UpDate()
+    {
+        GameObject[] allGo = FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allGo)
+        {
+            if (go.CompareTag("Monster"))
+                isReady = false;
+            else isReady = true;   
+        }
+    }
+}
